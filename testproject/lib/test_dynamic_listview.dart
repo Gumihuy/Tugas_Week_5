81cafa2ca5704d482d606c1ad587504149ae1c55
Gumihuy <masterikram2@gmail.com>
2024-03-20 22:36:17 +0700

    Tes

---
+++ testproject/lib/test_dynamic_listview.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class DynamicListView extends StatelessWidget {
+  DynamicListView({Key? key}) : super(key: key);
+
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dynamic ListView Example'),
+      ),
+      body: ListView(
+        children: numberList.map((number) {
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: Center(
+              child: Text(
+                '$number', // Ditampilkan sesuai item
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: DynamicListView(),
+  ));
+}
